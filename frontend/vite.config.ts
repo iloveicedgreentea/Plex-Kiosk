54451f20a82ab381f437ba5481927fccf7465b9b
iloveicedgreentea <31193909+iloveicedgreentea@users.noreply.github.com>
2025-01-17 22:44:00 -0500

    fix title

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -9,17 +9,5 @@ export default defineConfig({
       "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
-  server: {
-    proxy: {
-      '/api': {
-        target: '/api',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '')
-      },
-      '/thumbnail': {
-        target: '/thumbnail',  // This points to nginx on port 80
-        changeOrigin: true,
-      }
-    }
-  }
+
 })
